d64c19b72fa9ec80f01ef7e6fa55ed6697d5a11d
Roger Dodger <lpaddikt@gmail.com>
2018-07-18 22:12:39 -0700

    Finished battery class

--- Car_Classes/car.py
+++ Car_Classes/car.py
@@ -26,24 +26,29 @@ class Car:
         print('Your gas tank has been filled')
 
 
-class ElectricCar(Car):
-    def __init__(self, make, model, year):
-        super().__init__(make, model, year)
-        self.battery_size = 70
+class Battery:
+    def __init__(self, battery_size=70):
+        self.battery_size = battery_size
 
     def describe_battery(self):
         print('This car has a ' + str(self.battery_size) + ' Kwh battery')
 
+    def get_range(self):
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+        message = "This car can go approximately " + str(range) + " miles on a full charge"
+        print(message)
+
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
     def fill_gas_tank(self):
         print("You have an electric car so you don't need gas.")
-
-
-class Battery:
-    def __init__(self, battery_size = 70):
-        self.battery_size = battery_size
-
-    def describe_battery(self):
-        print("This battery has size of " + str(self.battery_size))
 
 
 new_gas_car = Car('Infiniti','G37', '2013')
@@ -53,4 +58,5 @@ new_gas_car.fill_gas_tank()
 new_electric_car = ElectricCar('Tesla', 'Model S', '2016')
 print(new_electric_car.get_descriptive_name())
 new_electric_car.fill_gas_tank()
-
+new_electric_car.battery.describe_battery()
+new_electric_car.battery.get_range()
